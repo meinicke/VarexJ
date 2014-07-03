10c6ebbd0fdba21c22e2eba70663e8d32a02d2b0
Meinicke <jensmeinicke@gmx.de>
2014-07-02 20:46:55 -0400

    add test to run BankAccount example

---
+++ src/tests/cmu/ExampleRunner.java
@@ -0,0 +1,24 @@
+package cmu;
+
+import gov.nasa.jpf.Config;
+import gov.nasa.jpf.JPF;
+import gov.nasa.jpf.util.test.TestJPF;
+
+import org.junit.Test;
+
+public class ExampleRunner extends TestJPF {
+
+	/**
+	 * Case Study: <b>Bank-Account</b><br>
+	 * #Feature: <b>10</b><br>
+	 * #Products: <b>144<b>
+	 */
+	@Test(timeout = 3000)
+	public void runBankAccount() {
+		// TODO revise (call example directly?)
+		Config config = JPF.createConfig(new String[] { "C:\\Users\\Loaner\\workspace\\jpf-core-vai\\src\\tests\\cmu\\BankAccount.jpf" });
+		JPF jpf = new JPF(config);
+		jpf.run();
+	}
+
+}
