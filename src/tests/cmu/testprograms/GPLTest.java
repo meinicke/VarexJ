00a4031cf885a641f1a7fbcb3938b644aea14eee
Meinicke <jensmeinicke@gmx.de>
2014-07-09 17:09:22 -0400

    add path output

--- src/tests/cmu/testprograms/GPLTest.java
+++ src/tests/cmu/testprograms/GPLTest.java
@@ -1,5 +1,7 @@
 package cmu.testprograms;
 
+import java.io.File;
+
 import gov.nasa.jpf.util.test.TestJPF;
 
 import org.junit.Ignore;
@@ -31,6 +33,8 @@ public class GPLTest extends TestJPF {
 	
 	
 	private void run(String graph) {
-		GPL.Main.main(new String[]{"Variability-Aware-Interpreter\\lib\\GPL\\" + graph, "v0"});
+		File f = new File("");
+		System.out.println("PATH: " + f.getAbsolutePath());
+		GPL.Main.main(new String[]{"lib\\GPL\\" + graph, "v0"});
 	}
 }
