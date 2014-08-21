4b585a3221f3d48c97a582408cf0ccc6236eb4f5
Meinicke <jensmeinicke@gmx.de>
2014-08-21 14:47:49 -0400

    fix bug to run AJStats test

--- src/tests/cmu/testprograms/AJStatsTest.java
+++ src/tests/cmu/testprograms/AJStatsTest.java
@@ -2,6 +2,7 @@ package cmu.testprograms;
 
 import gov.nasa.jpf.util.test.TestJPF;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AJStatsTest extends TestJPF {
@@ -9,12 +10,21 @@ public class AJStatsTest extends TestJPF {
 	private static final String FM = "";
 	private static String[] config = {"+search.class= .search.RandomSearch", "+classpath=lib\\AJStats.jar", FM};
 
-// 	@Ignore 
+ 	@Ignore 
  	@Test
-	public void ajStatsTest() {
+	public void ajStats1Test() {
 		if (verifyNoPropertyViolation(config)) {
 			if (ajs.FeatureSwitches.valid_product()) {
-				ajs.PL_Interface_impl.main(null);
+				ajs.PL_Interface_impl.main(new String[]{"1"});
+			}
+		}
+	}
+
+ 	@Test
+	public void ajStats2Test() {
+		if (verifyNoPropertyViolation(config)) {
+			if (ajs.FeatureSwitches.valid_product()) {
+				ajs.PL_Interface_impl.main(new String[]{"2"});
 			}
 		}
 	}
