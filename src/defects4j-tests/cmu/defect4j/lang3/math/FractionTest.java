5133eb5928dd9023a62f71feb9137360c37246f5
meinicke <jensmeinicke@gmx.de>
2015-02-17 19:08:55 +0100

    fix bug at FNEG.java ignore longrunning test from FractionTest.java

--- src/defects4j-tests/cmu/defect4j/lang3/math/FractionTest.java
+++ src/defects4j-tests/cmu/defect4j/lang3/math/FractionTest.java
@@ -1,11 +1,13 @@
 package cmu.defect4j.lang3.math;
 
 import gov.nasa.jpf.util.test.TestJPF;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class FractionTest extends TestJPF {
 
-    private final String[] config = {"+nhandler.delegateUnhandledNative", "+classpath+=${jpf-core}/lib/junit-4.11.jar,${jpf-core}/lib/commons-lang3-3.2-SNAPSHOT.jar,${jpf-core}/lib/easymock.jar,${jpf-core}/lib/commons-io.jar"};
+    private final String[] config = {"+nhandler.delegateUnhandledNative", "+classpath+=${jpf-core}/lib/junit-4.11.jar,${jpf-core}/lib/commons-lang3-3.2-SNAPSHOT.jar,${jpf-core}/lib/easymock.jar,${jpf-core}/lib/commons-io.jar", "+search.class= .search.RandomSearch"};
     public static void main(String[] testMethods){
         runTestsOfThisClass(testMethods);
     }
@@ -41,7 +43,8 @@ public class FractionTest extends TestJPF {
         }
     }
 
-    @Test(timeout=120000)
+    @Ignore// works but takes time
+    @Test//(timeout=120000)
     public void testFactory_double() throws Exception {
         if (verifyNoPropertyViolation(config)) {
                org.apache.commons.lang3.math.FractionTest object = new org.apache.commons.lang3.math.FractionTest();
