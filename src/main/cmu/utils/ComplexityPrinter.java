ac83bef1f797502c373c2061b2ad53b990f1e29d
meinicke <jensmeinicke@gmx.de>
2015-08-20 14:06:18 +0200

    some update

--- src/main/cmu/utils/ComplexityPrinter.java
+++ src/main/cmu/utils/ComplexityPrinter.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import cmu.conditional.Conditional;
@@ -34,30 +36,64 @@ public class ComplexityPrinter {
 		reset();
 	}
 	
+	static int complMax = 0;
+	static double complSum = 0;
+	static int featureMax = 0;
+	static double featureSum = 0;
+	
+	static int pointer = 0;
+	static final double samples = 1000;
 	public static void addComplex(int complexity, String instruction, FeatureExpr ctx, MethodInfo mi) {
-		line++;
-		if (line > 1_000_000) {
-			write();
-			line = 0;
-			results = new File("complex" + fileid++ + ".csv");
-			reset();
+		pointer++;
+		if (complexity > complMax ) {
+			complMax = complexity;
 		}
+		complSum += complexity;
+		if (ctx.collectDistinctFeatures().size() > featureMax ) {
+			featureMax = ctx.collectDistinctFeatures().size();
+		}
+		featureSum += ctx.collectDistinctFeatures().size();
 		
-		if (out != null) {
-			out.print(complexity);
-			out.print(';');
-//			out.print(instruction);
-//			out.print(';');
-//			out.print(mi.getName());
-//			out.print(';');
-			out.print(ctx.collectDistinctFeatures().size());
-//			out.print(';');
-//			out.print(Conditional.getCTXString(ctx));
-			out.println();
+		if (pointer >= samples) {
+			pointer = 0;
+			line++;
+			if (line > 1_000_000) {
+				write();
+				line = 0;
+				results = new File("complex" + fileid++ + ".csv");
+				reset();
+			}
+			
+			if (out != null) {
+				out.print(complMax);
+				out.print(';');
+				out.print(String.valueOf(complSum / samples).replace('.', ','));
+				out.print(';');
+				out.print(featureMax);
+				out.print(';');
+				out.print(String.valueOf(featureSum / samples).replace('.', ','));
+				out.println();
+				
+				complMax = 0;
+				complSum = 0;
+				featureMax = 0;
+				featureSum = 0;
+//				out.print(complexity);
+//				out.print(';');
+//				out.print(instruction);
+//				out.print(';');
+//				out.print(mi.getName());
+//				out.print(';');
+//				out.print(ctx.collectDistinctFeatures().size());
+//				out.print(';');
+//				out.print(Conditional.getCTXString(ctx));
+//				out.println();
+			}
 		}
 	}
 
 	public static void reset() {
+		
 		try {
 			out = new PrintWriter(new BufferedWriter(new FileWriter(results, true)));
 		} catch (IOException e) {
@@ -77,8 +113,10 @@ public class ComplexityPrinter {
 
 
 	public static void write() {
-		out.flush();
-		out.close();
-		out = null;
+		if (out != null) {
+			out.flush();
+			out.close();
+			out = null;
+		}
 	}
 }
