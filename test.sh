559538733532d47babd0ccd40c55437ab4f1f61a
meinicke <jensmeinicke@gmx.de>
2015-11-27 14:41:45 +0100

    remove unnecessary files from root

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ant -buildfile VarexJ/build.xml clean
-ant -buildfile VarexJ/jpf-nhandler/build.xml clean
-ant -buildfile VarexJ/build.xml build
-ant -buildfile VarexJ/jpf-nhandler/build.xml build
-ant -buildfile VarexJ/build.xml test
