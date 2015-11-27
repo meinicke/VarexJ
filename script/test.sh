e500930560184afee9517e18b9cc567d458770cf
meinicke <jensmeinicke@gmx.de>
2015-11-27 14:40:25 +0100

    fix path in test.sh

--- script/test.sh
+++ script/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ant -buildfile VarexJ/build.xml clean
-ant -buildfile VarexJ/jpf-nhandler/build.xml clean
+ant -buildfile jpf-nhandler/build.xml clean
 ant -buildfile VarexJ/build.xml build
-ant -buildfile VarexJ/jpf-nhandler/build.xml build
+ant -buildfile jpf-nhandler/build.xml build
 ant -buildfile VarexJ/build.xml test
