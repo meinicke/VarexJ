2751bd49f299598177ad97638213ed6c1a7a70ad
meinicke <jensmeinicke@gmx.de>
2015-11-27 14:21:31 +0100

    travis ci setup

--- script/test.sh
+++ script/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#ant -buildfile VarexJ/build.xml clean --verbose
-#ant -buildfile VarexJ/jpf-nhandler/build.xml clean --verbose
-#ant -buildfile VarexJ/build.xml build --verbose
-#ant -buildfile VarexJ/jpf-nhandler/build.xml build --verbose
-#ant -buildfile VarexJ/build.xml test --verbose
+ant -buildfile VarexJ/build.xml clean --verbose
+ant -buildfile VarexJ/jpf-nhandler/build.xml clean --verbose
+ant -buildfile VarexJ/build.xml build --verbose
+ant -buildfile VarexJ/jpf-nhandler/build.xml build --verbose
+ant -buildfile VarexJ/build.xml test --verbose
