5bc2b5e76a21c93a8540fc3bc95ef51bc6a0a6b5
meinicke <jensmeinicke@gmx.de>
2015-11-27 13:50:32 +0100

    first step to new project structure

--- script/test.sh
+++ script/test.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-
-#clean
 ant -buildfile VarexJ/build.xml clean
 ant -buildfile VarexJ/jpf-nhandler/build.xml clean
-
-#build
 ant -buildfile VarexJ/build.xml build
 ant -buildfile VarexJ/jpf-nhandler/build.xml build
-
-#test
 ant -buildfile VarexJ/build.xml test
