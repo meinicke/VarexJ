698430b2f876bf80dd23e816388c62e5cd9a846a
Jens Meinicke <jensmeinicke@gmx.de>
2016-10-10 21:01:16 -0400

    add Interface JavaObjectInputStreamAccess in classes for Travis build

---
+++ VarexJ/src/classes/java/sun/misc/JavaObjectInputStreamAccess.java
@@ -0,0 +1,6 @@
+package sun.misc;
+
+public abstract interface JavaObjectInputStreamAccess {
+
+	public abstract void setValidator(java.io.ObjectInputStream arg0, sun.misc.ObjectStreamClassValidator arg1);
+}
