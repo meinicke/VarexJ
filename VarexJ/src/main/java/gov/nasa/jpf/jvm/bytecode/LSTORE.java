34ecb58cda8d4552a0f7fc090a251ab8b56bf26c
Jens Meinicke <meinicke@ovgu.de>
2017-06-29 12:03:46 -0700

    add log for FSTORE and LSTORE

--- VarexJ/src/main/java/gov/nasa/jpf/jvm/bytecode/LSTORE.java
+++ VarexJ/src/main/java/gov/nasa/jpf/jvm/bytecode/LSTORE.java
@@ -19,10 +19,13 @@
 package gov.nasa.jpf.jvm.bytecode;
 
 import cmu.conditional.Conditional;
+import cmu.vatrace.LocalStoreStatement;
 import de.fosd.typechef.featureexpr.FeatureExpr;
 import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.LocalVarInfo;
 import gov.nasa.jpf.vm.StackFrame;
 import gov.nasa.jpf.vm.ThreadInfo;
+import gov.nasa.jpf.vm.Types;
 
 
 /**
@@ -39,7 +42,28 @@ public class LSTORE extends LocalVariableInstruction implements StoreInstruction
   public Conditional<Instruction> execute (FeatureExpr ctx, ThreadInfo ti) {
     StackFrame frame = ti.getModifiableTopFrame();
     
-    frame.storeLongOperand(ctx, index);
+    LocalVarInfo localVarInfo = frame.getLocalVarInfo(index, ctx);
+    Conditional<Double> oldValue = null;
+    if (localVarInfo != null) {
+		int startPC = localVarInfo.getStartPC();
+		int pos = getPosition();
+		if (pos == startPC - 1) {// init
+			oldValue = null;
+		} else {
+			Conditional<Integer> oldValue1 = frame.getLocalVariable(frame.stack.getCtx(), index);
+			Conditional<Integer> oldValue2 = frame.getLocalVariable(frame.stack.getCtx(), index + 1);
+			oldValue = oldValue1.mapr(h -> oldValue2.map(l -> Types.longToDouble(Types.intsToLong(l, h)))).simplify();
+		}
+    }
+    
+	frame.storeLongOperand(ctx, index);
+	
+	if (localVarInfo != null) {
+		Conditional<Integer> newValue1 = frame.getLocalVariable(frame.stack.getCtx(), index);
+		Conditional<Integer> newValue2 = frame.getLocalVariable(frame.stack.getCtx(), index + 1);
+		Conditional<Double> newValue = newValue1.mapr(h -> newValue2.map(l -> Types.longToDouble(Types.intsToLong(l, h)))).simplify();
+		new LocalStoreStatement(this, frame.method, oldValue, newValue, localVarInfo, ctx);
+	}
     
     return getNext(ctx, ti);
   }
