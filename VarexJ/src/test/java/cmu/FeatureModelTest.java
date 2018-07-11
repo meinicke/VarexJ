6fec04b5b429c23a61b2b3f804ba53c969670f54
Jens Meinicke <meinicke@ovgu.de>
2018-07-11 15:13:04 -0700

    fix FeatureModelTest

--- VarexJ/src/test/java/cmu/FeatureModelTest.java
+++ VarexJ/src/test/java/cmu/FeatureModelTest.java
@@ -20,30 +20,26 @@ import de.fosd.typechef.featureexpr.FeatureModel;
  */
 public class FeatureModelTest {
 	
+	private static final String ELEVATOR_DIMACS = "elevator.dimacs";
+	private static final String B_AMODEL_DIMACS = "BAmodel.dimacs";
+	private static final String MODEL_DIMACS = "model.dimacs";
+	
 	private final static String SEPARATOR = File.separator;
 	private final static String RES_PATH = "src" + SEPARATOR + "test" + SEPARATOR + "resources" + SEPARATOR;
 	private final static File PATH;
 	static {
 		PATH = new File(RES_PATH);
-		System.out.println("TEST FILE EXISTS");
-		System.out.println(PATH.getPath() + " " + PATH.exists());
-		System.out.println(PATH.getAbsoluteFile());
-		File[] children = PATH.listFiles();
-		for (File file : children) {
-			System.out.println(file.getAbsolutePath());
-		}
 	}
 	
 	@Test
 	public void testSimpleModel() throws Exception {
-		System.out.println("FeatureModelTest.testSimpleModel()");
 		FeatureExprFactory.setDefault(FeatureExprFactory.sat());
 		
 		FeatureExpr a = FeatureExprFactory.createDefinedExternal("CONFIG_A");
 		FeatureExpr b = FeatureExprFactory.createDefinedExternal("CONFIG_B");
 		FeatureExpr c = FeatureExprFactory.createDefinedExternal("CONFIG_C");
 		
-		FeatureModel model = FeatureExprFactory.dflt().featureModelFactory().createFromDimacsFile(new File(PATH, "model.dimacs").getPath().toString());
+		FeatureModel model = FeatureExprFactory.dflt().featureModelFactory().createFromDimacsFile(new File(PATH, MODEL_DIMACS).getPath().toString());
 		assertFalse(a.and(b.not()).isContradiction());
 		assertTrue(a.and(b.not()).isContradiction(model));
 		
@@ -53,10 +49,9 @@ public class FeatureModelTest {
 	
 	@Test
 	public void testBankAccount() throws Exception {
-		System.out.println("FeatureModelTest.testBankAccount()");
 		FeatureExprFactory.setDefault(FeatureExprFactory.bdd());
 		FeatureExpr bankaccount = FeatureExprFactory.createDefinedExternal("CONFIG_bankaccount");
-		FeatureModel model = FeatureExprFactory.dflt().featureModelFactory().createFromDimacsFile(new File(PATH, "BAmodel.dimacs").getPath().toString());
+		FeatureModel model = FeatureExprFactory.dflt().featureModelFactory().createFromDimacsFile(new File(PATH, B_AMODEL_DIMACS).getPath().toString());
 		assertFalse(bankaccount.not().isContradiction());
 		assertTrue(bankaccount.not().isContradiction(model));
 		
@@ -64,23 +59,21 @@ public class FeatureModelTest {
 	
 	@Test
 	public void testElevator() throws Exception {
-		System.out.println("FeatureModelTest.testElevator()");
 		FeatureExprFactory.setDefault(FeatureExprFactory.bdd());
 		FeatureExpr Base = FeatureExprFactory.createDefinedExternal("CONFIG_Base");
-		FeatureModel model = FeatureExprFactory.dflt().featureModelFactory().createFromDimacsFile(new File(PATH, "Elevator.dimacs").getPath().toString());
+		FeatureModel model = FeatureExprFactory.dflt().featureModelFactory().createFromDimacsFile(new File(PATH, ELEVATOR_DIMACS).getPath().toString());
 		assertFalse(Base.not().isContradiction());
 		assertTrue(Base.not().isContradiction(model));
 	}
 	
 	@Test
 	public void testElevator2() throws Exception {
-		System.out.println("FeatureModelTest.testElevator2()");
 		FeatureExprFactory.setDefault(FeatureExprFactory.bdd());
 		FeatureExpr TwoThirdsFull = FeatureExprFactory.createDefinedExternal("CONFIG_TwoThirdsFull");
 		FeatureExpr ExecutiveFloor = FeatureExprFactory.createDefinedExternal("CONFIG_ExecutiveFloor");
 		FeatureExpr Weight = FeatureExprFactory.createDefinedExternal("CONFIG_Weight");
 		FeatureExpr Overloaded = FeatureExprFactory.createDefinedExternal("CONFIG_Overloaded");
-		FeatureModel model = FeatureExprFactory.dflt().featureModelFactory().createFromDimacsFile(new File(PATH, "Elevator.dimacs").getPath().toString());
+		FeatureModel model = FeatureExprFactory.dflt().featureModelFactory().createFromDimacsFile(new File(PATH, ELEVATOR_DIMACS).getPath().toString());
 		FeatureExpr ctx = FeatureExprFactory.True();
 		ctx = ctx.and(Overloaded);
 		ctx = ctx.and(ExecutiveFloor.not());
@@ -88,7 +81,7 @@ public class FeatureModelTest {
 		ctx = ctx.and(TwoThirdsFull.not());
 		assertFalse(ctx.isContradiction());
 		assertTrue(ctx.isContradiction(model));
-		Conditional.setFM("src/test/resources/Elevator.dimacs");
+		Conditional.setFM("src/test/resources/elevator.dimacs");
 	}
 	
 }
